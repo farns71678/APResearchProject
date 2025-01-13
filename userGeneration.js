7ef1f83fe36f6cdfc377d9c5f6f9b2664b246557
farns71678 <142346942+farns71678@users.noreply.github.com>
2025-01-13 15:51:30 +0000

    Birth date

--- userGeneration.js
+++ userGeneration.js
@@ -1,1 +1,27 @@
-let app = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+console.log(randomBirthDate());
+
+app.get("/", (req, res) => {
+    res.send("Random User: " + randomBirthDate());
+});
+
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:" + port);
+});
+
+function createUser() {
+    // gender: false = male, true = female
+    let user = { name: "", birthdate: "", email: "", ssn: 0, gender: false};
+}
+
+// creates a random birth date from 1940 to 2040
+function randomBirthDate() {
+    // range 1940 to 2040
+    let year = Math.floor(40 + Math.random() * 100);
+    let month = Math.floor(Math.random() * 12);
+    let date = Math.floor(Math.random() * 30.5);
+    return (new Date(Date.UTC(year, month, date))).toISOString();
+}
