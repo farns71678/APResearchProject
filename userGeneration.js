2d8afd095ec16c8ed91f5ddcce8c18509eacd04d
farns71678 <142346942+farns71678@users.noreply.github.com>
2025-01-14 15:44:02 +0000

    Read File Data

--- userGeneration.js
+++ userGeneration.js
@@ -1,11 +1,18 @@
+const fs = require('node:fs/promises');
 const express = require('express');
 const app = express();
 const port = 3000;
+let maleData = null;
+let femaleData = null;
 
 console.log(randomBirthDate());
 
-app.get("/", (req, res) => {
-    res.send("Random User: " + randomBirthDate());
+app.get("/", async (req, res) => {
+    //res.send("Random User: " + randomBirthDate());
+    maleData = await readMaleFirstNames();
+    femaleData = await readFemaleFirstNames();
+    
+    res.send(maleData + " <br><br> " + femaleData);
 });
 
 app.listen(port, () => {
@@ -32,4 +39,32 @@ function randomSSN() {
 
 function randomGender() {
     return (Math.random() * 2 < 1);
+}
+
+// generates random name (first and last)
+// first names are created using the top 1000 names in US from https://www.thenamegeek.com/most-common-female-names based off of data from SSA
+function randomName() {
+    
+}
+
+async function readMaleFirstNames() {
+    try {
+        let data = await fs.readFile(__dirname + "/maleNames.csv", {encoding: 'utf-8'});
+        return data;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+async function readFemaleFirstNames() {
+    try {
+        let data = await fs.readFile(__dirname + "/femaleNames.csv", {encoding: 'utf-8'});
+        return data;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
 }
